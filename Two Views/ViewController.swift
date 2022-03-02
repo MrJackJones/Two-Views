c862a86708bc611a2be034694cfb25d6eb386549
back2 <back2@local.ru>
2022-03-02 12:30:18 +0300

    Initial Commit

---
+++ Two Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Views
+//
+//  Created by Ivan on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
